941153fd5ac8176aa527454c2e1fcc2237d4feea
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-11-09 09:42:57 -0800

    spotify api template

--- client/src/config-folder/favQs_example_api_key.js
+++ client/src/config-folder/favQs_example_api_key.js
@@ -1,10 +1,10 @@
-// Your FavQs API keys here!
+// Your FavQs API keys go here!
 // Sign up for your API key: https://favqs.com/api_keys
 module.exports = {
   FAVQS_API_KEY: 'INSERT API KEY HERE'
 }
 
-// Once API key is inserted as a string, rename file
+// Once API key is inserted as a string, please rename file
 // favQs_example_api_key.js --> favQs_api_key.js
 
-// git add, git commit and npm start :)
+// Remember to git add, git commit and npm start :)
