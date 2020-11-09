5aee46b14d417c87f4170521d3333bd66b12a527
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-11-09 09:53:49 -0800

    twitter api template

---
+++ client/src/config-folder/twitter_example_api_key.js
@@ -0,0 +1,13 @@
+// Your Twitter API key go here!
+// Sign up for your API key: https://developer.twitter.com/en/docs/twitter-api
+
+module.exports = {
+  TWITTER_API_KEY: 'INSERT API KEY HERE',
+  TWITTER_SECRET_KEY: 'INSERT SECRET KEY HERE',
+  TWITTER_BEARER_TOKEN: 'INSERT BEARER TOKEN HERE'
+}
+
+// Once API keys are inserted as a string, please rename file
+// twitter_example_api_key.js --> twitter_api_key.js
+
+// Remember to git add, git commit and npm start :)
